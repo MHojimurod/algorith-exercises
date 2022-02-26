4f29dcd88f1918638fe9e7c5e22b85805eca200f
MHojimurod <cool.hojimurod2001@mail.ru>
2022-02-26 09:22:16 +0500

    bat file push

---
+++ 1TwoSum.py
@@ -0,0 +1,33 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# You can return the answer in any order.
+
+
+# Example 1:
+
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+# Example 2:
+
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+
+# Example 3:
+
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        data = []
+        for i in nums:
+            if target-i in nums[nums.index(i)+1:]:
+                if i == target-i:
+                    data = [nums.index(i), nums[nums.index(i)+1:].index(target-i)+len(nums[:nums.index(i)+1])]
+                else:
+                    data = [nums.index(i), nums.index(target-i)]
+        return data
---
+++ push.bat
@@ -0,0 +1,8 @@
+@echo off
+
+
+git add .
+git status
+git commit -m "bat file push"
+git pull
+git push
